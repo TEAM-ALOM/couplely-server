8376214e35074efcd425ce9a2f4bc756fc492315
이수현 <soohyun@isuhyeon-ui-MacBookAir.local>
2023-11-02 17:45:03 +0900

    first test

--- src/main/java/com/example/couplelyserver/CouplelyServerApplication.java
+++ src/main/java/com/example/couplelyserver/CouplelyServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CouplelyServerApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(CouplelyServerApplication.class, args);
     }
